4cdab446a5ec67b2958de14abc05fc24b37ae031
maydurya <maydurya@gmail.com>
2024-01-23 00:15:08 +0300

    cw ended

--- index.js
+++ index.js
@@ -10,9 +10,21 @@ let num2 = 33;
 
 console.log(9 % 4);
 console.log('A'.charCodeAt());
-let str = prompt('Введите строку');
+// let str = prompt('Введите строку');
 // let f = str || 'Ничего не введено'; Ложные значения - 0 '' null undefined
 // let f = str ?? 'Ничего не введено'; // ?? - оператор нулевого слияния
 
-let f = 'Ничего не введено' && str && null;
-console.log(f);
+// let f = 'Ничего не введено' && str && null;
+// console.log(f);
+
+let num3 = 45;
+
+num3 += 20; //num3 = num3 + 20;
+
+let m = 234;
+
+m = m + '2';
+
+console.log(m);
+
+let str2 = '123124';
