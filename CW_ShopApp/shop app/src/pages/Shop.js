df4e1c53f0da56c06f608b0cb52097a9ddb88c54
maydurya <maydurya@gmail.com>
2024-03-25 21:43:04 +0300

    shop app started

---
+++ CW_ShopApp/shop app/src/pages/Shop.js
@@ -0,0 +1,33 @@
+import '../../style/shop.scss';
+import ShopItem from '../components/ShopItem';
+import { getData } from '../utils/api';
+
+export default class Shop {
+	constructor() {
+		this.elem = document.createElement('div');
+		this.elem.classList.add('container');
+	}
+
+	async getContent() {
+		const data = await getData();
+		if (!data) return;
+
+		const header = document.createElement('h2');
+		header.classList.add('title');
+		header.innerHTML = 'Shop';
+		const items = document.createElement('div');
+		items.classList.add('items');
+
+		data.forEach(data => {
+			const item = new ShopItem(data).render();
+			items.append(item);
+		});
+
+		this.elem.append(header, items);
+	}
+
+	render() {
+		this.getContent();
+		return this.elem;
+	}
+}
